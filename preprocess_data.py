81c28751fcec22b1ccdb6245bc8cd634836c671a
Delal Tomruk <66208179+delaltomruk@users.noreply.github.com>
2021-05-13 18:30:52 +0300

    Add files via upload

---
+++ preprocess_data.py
@@ -0,0 +1,28 @@
+def preprocess_data(df):
+    df["SaleYear"] = df.saledate.dt.year
+    df["SaleMonth"] = df.saledate.dt.month
+    df["SaleDay"] = df.saledate.dt.day
+    df["DayOfWeek"] = df.saledate.dt.dayofweek
+    df["DayOfYear"] = df.saledate.dt.dayofyear
+    df.drop("saledate", axis = 1, inplace = True)
+    
+    relevant_features = ["SalePrice", "MachineID", "SaleYear", "YearMade", "DayOfWeek"]
+    cat_cols = list(set(df.columns) - set(df._get_numeric_data().columns))
+    cols_to_drop = set(df.columns) - set(cat_cols + relevant_num)
+
+    for i in cols_to_drop:
+        df.drop(i, axis = 1, inplace = True)
+        
+    df = df.reset_index(drop=True)
+    
+    # categorical
+    for label, content in df.items():
+        if not pd.api.types.is_numeric_dtype(content):
+            df[label] = pd.Categorical(content).codes + 1   
+    
+    # handling numerical missing values for df_train
+    missing_num_train = list(set(df.columns) - set(missing_cat_train))
+    for i in missing_num_train:
+        df[i].fillna(df[i].mean(), inplace=True)
+        
+    return df
